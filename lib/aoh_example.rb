4f4df11919491a9185b2a509d0a31149de3f164d
Winston McGriff <winstonmcgriff27@gmail.com>
2021-02-19 04:53:03 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -21,7 +21,7 @@ def assembled_aoh
 end
 
 def literal_aoh
-a_single_array =  [
+a_single_array = [
 {:name =>"Don Gately", :occupation =>"Live-in Staff" },
 {:name =>"Joelle van Dyne", :occupation =>"Radio Personality"},
 {:name =>"Pat Monteseian", :occupation =>"Staff" },
